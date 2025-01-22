2ade5f6183cd9cddf18f970c83860e263a26a411
Wesley Maa <wesley.maa@gmail.com>
2025-01-23 05:25:52 +0800

    try

--- imu/bindings/src/bin/stub_gen.rs
+++ imu/bindings/src/bin/stub_gen.rs
@@ -1,7 +1,13 @@
+use pyo3::prelude::*;
 use pyo3_stub_gen::Result;
 
 fn main() -> Result<()> {
-    let stub = bindings::stub_info()?;
-    stub.generate()?;
-    Ok(())
+    // Initialize the Python interpreter
+    pyo3::prepare_freethreaded_python();
+    
+    Python::with_gil(|_py| {
+        let stub = bindings::stub_info()?;
+        stub.generate()?;
+        Ok(())
+    })
 }
