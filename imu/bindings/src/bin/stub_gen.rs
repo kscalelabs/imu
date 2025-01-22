13bfdb203bd64f1fb24917509f6cbbdfbe0db73c
Wesley Maa <wesley.maa@gmail.com>
2025-01-23 05:33:00 +0800

    fix

--- imu/bindings/src/bin/stub_gen.rs
+++ imu/bindings/src/bin/stub_gen.rs
@@ -1,13 +1,7 @@
-use pyo3::prelude::*;
 use pyo3_stub_gen::Result;
 
 fn main() -> Result<()> {
-    // Initialize the Python interpreter
-    pyo3::prepare_freethreaded_python();
-    
-    Python::with_gil(|_py| {
-        let stub = bindings::stub_info()?;
-        stub.generate()?;
-        Ok(())
-    })
+    let stub = bindings::stub_info()?;
+    stub.generate()?;
+    Ok(())
 }
