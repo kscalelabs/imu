57ced3240752a9f6ef8a4bb8557e1e91b0da8e4c
Ali Kuwajerwala <ali@kscale.dev>
2025-02-10 16:55:34 -0800

    remove build file

--- imu/bindings/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    // Tell cargo to look for Python libraries in the conda env
-    println!("cargo:rustc-link-search=/home/kuwajerw/anaconda3/envs/zdemos/lib");
-    
-    // Link against Python library (name might be python3.x or python3.x.so)
-    println!("cargo:rustc-link-lib=python3.11");
-} 
