40e2e8e756718b94730aaa300c7366f8e753775a
Wesley Maa <wesley.maa@gmail.com>
2025-04-23 13:43:04 -0700

    fmt" ;

--- imu/src/lib.rs
+++ imu/src/lib.rs
@@ -5,5 +5,5 @@ pub use linux_bmi088::Bmi088Reader;
 #[cfg(target_os = "linux")]
 pub use linux_bno055::Bno055Reader;
 
-pub use imu_traits::*;
 pub use hiwonder::HiwonderReader;
+pub use imu_traits::*;
